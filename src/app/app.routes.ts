2bc49eade004e957c0c4d48f77a3a1098fb8e705
Alex Fraser <alex.d.fraser@gmail.com>
2023-12-17 12:42:03 +0000

    Added a route to an about page and server-side rendering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+// import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'about', loadComponent: () => import('./about/about.component').then(mod => mod.AboutComponent) }];
+// export const routes: Routes = [{ path: 'about', component: AboutComponent }];
